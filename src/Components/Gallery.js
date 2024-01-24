da9cdc016f970eb891ddf7f121d1ee9ec834f8a8
Akshay Barve <akshaybarve01@gmail.com>
2024-01-24 18:40:19 +0530

    Image added

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,57 +1,98 @@
-import React from 'react'
-import "./css/Gallery.css"
+import React from "react";
+import "./css/Gallery.css";
 
 const Gallery = () => {
   return (
     <>
-    <div className="container-fluid">
-    <div className="first">
-        
-        <div className="one">
+      <div className="container-fluid">
+        <div className="first">
+          <div className="one">
             <img src="./images2/gallery1.jpeg" alt="" />
-        </div>
-        <div className="one">
+          </div>
+          <div className="one">
             <img src="./images2/gallery17.jpeg" alt="" />
-        </div>
-        <div className="one">
+          </div>
+          <div className="one">
             <img src="./images2/gallery18.jpeg" alt="" />
-        </div>
-        <div className="one">
+          </div>
+          <div className="one">
             <img src="./images2/gallery4.jpeg" alt="" />
-        </div>
-    </div>
-        <div className="first">
-        
-            <div className="one">
-                <img src="./images2/gallery5.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery6.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery16.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery8.jpeg" alt="" />
-            </div>
+          </div>
         </div>
         <div className="first">
-            <div className="one">
-                <img src="./images2/gallery9.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery10.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery19.jpeg" alt="" />
-            </div>
-            <div className="one">
-                <img src="./images2/gallery14.jpeg" alt="" />
-            </div>
+          <div className="one">
+            <img src="./image3/g1.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g2.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g3.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g4.jpeg" alt="" />
+          </div>
         </div>
-    </div>
-    </>
-  )
-}
 
-export default Gallery
+        <div className="first">
+          <div className="one">
+            <img src="./images2/gallery5.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery6.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery16.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery8.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="first">
+          <div className="one">
+            <img src="./image3/g5.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g6.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g7.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g8.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="first">
+          <div className="one">
+            <img src="./image3/g9.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g10.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g11.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./image3/g12.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="first">
+          <div className="one">
+            <img src="./images2/gallery9.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery10.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery19.jpeg" alt="" />
+          </div>
+          <div className="one">
+            <img src="./images2/gallery14.jpeg" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
